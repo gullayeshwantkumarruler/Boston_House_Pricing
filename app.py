5bc2523cdef26e904503f86f3bd07a07dc421f8f
Yeshwant <gullayeshwantkumarruler@gmail.com>
2022-09-08 15:01:21 +0530

    web app is created

--- app.py
+++ app.py
@@ -21,6 +21,15 @@ def predict_api():
     print(prediction[0])   # as output is like a 2 d array so indexing 0
     return jsonify({'prediction':prediction[0]})
 
+@app.route('/predict',methods=['POST'])  # prediction page
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    prediction=regmodel.predict(final_input)[0]
+    print(prediction)
+    return render_template('home.html',prediction_text='The predicted House Price is {}'.format(prediction))
+
+
 if __name__=='__main__':
     app.run(debug=True)
-    
